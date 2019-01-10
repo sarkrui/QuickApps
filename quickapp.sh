6753279d4989c7b564c66f2a2e305b136d83ac22
Sark XING <sark.rui@gmail.com>
2019-01-10 11:56:26 -0800

    chrome dropbox wechat whatsapp sublime-text the-unarchiver arduino processing github telegram geekbench qq teamviewer wget typora

---
+++ quickapp.sh
@@ -0,0 +1,3 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install chrome dropbox wechat whatsapp sublime-text the-unarchiver arduino processing github telegram geekbench qq teamviewer wget typora 
